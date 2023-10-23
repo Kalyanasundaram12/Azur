5a9a214d96072fcd566ffa6586505c04c7921c06
AneeshVish <100186094+AneeshVish@users.noreply.github.com>
2023-10-23 18:57:47 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
-import { AboutUsComponent } from './about-us/about-us.component';
-import { ContactUsComponent } from './contact-us/contact-us.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
-    AboutUsComponent,
-    ContactUsComponent
   ],
   imports: [
     BrowserModule,
