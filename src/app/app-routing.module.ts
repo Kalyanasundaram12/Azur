e1ba00a4a153549e66bb5ba7f46844fbb77ad617
AneeshVish <100186094+AneeshVish@users.noreply.github.com>
2023-10-23 18:57:20 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
-import { AboutUsComponent } from './about-us/about-us.component';
 
 const routes: Routes = [
   { 
     path: '', 
     component: MainPageComponent
-  },
-  { path: 'about-us', 
-    component: AboutUsComponent
   },
 ];
 
